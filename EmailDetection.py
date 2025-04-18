70f4b77d7ceea51380dbb9e91b2bc2af6c362d13
Arjun Arundiyar <ramesharjun287@gmail.com>
2025-04-18 17:03:39 +0530

    final version

--- EmailDetection.py
+++ EmailDetection.py
@@ -6,7 +6,7 @@ import string
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import accuracy_score
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
